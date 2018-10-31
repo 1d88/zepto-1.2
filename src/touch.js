5e1d68bd189e5fb267f230c1024ffb1aa11e4ac0
cuihongquan <cuihongquan@soyoung.com>
2018-10-31 14:39:32 +0800

    touch

--- src/touch.js
+++ src/touch.js
@@ -11,6 +11,10 @@
     eventMap,
     initialized = false
 
+    /* 
+      滑动方向
+      即使有其它轴的偏移，也忽略掉
+    */
   function swipeDirection(x1, x2, y1, y2) {
     return Math.abs(x1 - x2) >=
       Math.abs(y1 - y2) ? (x1 - x2 > 0 ? 'Left' : 'Right') : (y1 - y2 > 0 ? 'Up' : 'Down')
@@ -38,12 +42,14 @@
     touch = {}
   }
 
+  // 是不是touch
   function isPrimaryTouch(event){
     return (event.pointerType == 'touch' ||
       event.pointerType == event.MSPOINTER_TYPE_TOUCH)
       && event.isPrimary
   }
 
+  // 检测是不是pointer 类型的事件
   function isPointerEventType(e, type){
     return (e.type == 'pointer'+type ||
       e.type.toLowerCase() == 'mspointer'+type)
@@ -67,12 +73,15 @@
     unregisterTouchEvents()
 
     eventMap = (__eventMap && ('down' in __eventMap)) ? __eventMap :
+      // chrome safari
       ('ontouchstart' in document ?
       { 'down': 'touchstart', 'up': 'touchend',
         'move': 'touchmove', 'cancel': 'touchcancel' } :
+        // ie
       'onpointerdown' in document ?
       { 'down': 'pointerdown', 'up': 'pointerup',
         'move': 'pointermove', 'cancel': 'pointercancel' } :
+        // ie
        'onmspointerdown' in document ?
       { 'down': 'MSPointerDown', 'up': 'MSPointerUp',
         'move': 'MSPointerMove', 'cancel': 'MSPointerCancel' } : false)
@@ -80,6 +89,7 @@
     // No API availables for touch events
     if (!eventMap) return
 
+    // 能力检测，如果是IE的设备
     if ('MSGesture' in window) {
       gesture = new MSGesture()
       gesture.target = document.body
@@ -95,10 +105,20 @@
         })
     }
 
+    // 点击下 动作如 touchstart 的时候
+    /* 
+      1、存储当前点击的坐标
+      2、通过touch.last 判断是双击还是单击
+      3、触发longTab的事件
+    */
+
     down = function(e){
       if((_isPointerType = isPointerEventType(e, 'down')) &&
         !isPrimaryTouch(e)) return
+      // 第一个touch点
       firstTouch = _isPointerType ? e : e.touches[0]
+
+
       if (e.touches && e.touches.length === 1 && touch.x2) {
         // Clear out touch movement data if we have it sticking around
         // This can occur if touchcancel doesn't fire due to preventDefault, etc.
@@ -106,19 +126,30 @@
         touch.y2 = undefined
       }
       now = Date.now()
+      // 时间延迟
       delta = now - (touch.last || now)
+      // 能力检测当前有没有tagName 看是否是一个 element元素，如果不是，那么拿取它的父级元素
       touch.el = $('tagName' in firstTouch.target ?
         firstTouch.target : firstTouch.target.parentNode)
+      // 点击超时函数
       touchTimeout && clearTimeout(touchTimeout)
+      // 拿到点击的 初始点
       touch.x1 = firstTouch.pageX
       touch.y1 = firstTouch.pageY
+      // 如果上次和 本次的点击时间差在150秒之内，标识touch为doubleTop
       if (delta > 0 && delta <= 250) touch.isDoubleTap = true
+      // 更新这次点击，缓存为上次点击的时间
       touch.last = now
       longTapTimeout = setTimeout(longTap, longTapDelay)
-      // adds the current touch contact for IE gesture recognition
+      // adds the current touch contact for IE gesture recognition ====IEIEIIE
       if (gesture && _isPointerType) gesture.addPointer(e.pointerId)
     }
 
+    /* 
+     touchmove move 的时候会取消掉longTap
+      1、取消当前longTap事件
+      2、计算当前的坐标偏移量
+    */
     move = function(e){
       if((_isPointerType = isPointerEventType(e, 'move')) &&
         !isPrimaryTouch(e)) return
@@ -131,6 +162,14 @@
       deltaY += Math.abs(touch.y1 - touch.y2)
     }
 
+    // touchend的时机
+    /* 
+      1、取消当前longTap事件
+      2、判断是否是swipe动作，动作最小偏移量为 30像素
+      3、判断是否是tap点击， 点击范围不超过30像素，前提没有滚动
+      4、判断是否是dbtap点击
+      5、判断是否是singTap点击，如果是延迟250毫秒执行
+    */
     up = function(e){
       if((_isPointerType = isPointerEventType(e, 'up')) &&
         !isPrimaryTouch(e)) return
@@ -152,6 +191,7 @@
       else if ('last' in touch)
         // don't fire tap when delta position changed by more than 30 pixels,
         // for instance when moving to a point and back to origin
+        // 如何断定是一个tap
         if (deltaX < 30 && deltaY < 30) {
           // delay by one tick so we can cancel the 'tap' event if 'scroll' fires
           // ('tap' fires before 'scroll')
